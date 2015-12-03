95b02efd8f408cfa4710a05d5c0e8f6f56245ab0
flaver12 <flaverkleiber@yahoo.de>
2015-12-03 14:31:41 +0100

    Startet with the serialization stuff for save game

---
+++ Assets/Scripts/SaveAndLoad/Serialization.cs
@@ -0,0 +1,27 @@
+﻿/**
+* Serialization class for our save games
+*
+* @author Flavio Kleiber
+* @copyright (c) 2015 PiratesAndChees Studios 
+*/
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Serialization  {
+
+	public static BinaryFormatter binaryFormatter = new BinaryFormatter ();
+
+	public static void save(string key, object value) {
+		MemoryStream memoryStream = new MemoryStream ();
+		binaryFormatter.Serialize(memoryStream, value);
+		string temp = System.Convert.ToBase64String(memoryStream.ToArray());
+		PlayerPrefs.SetString(key, temp);
+	}
+
+	public static object load() {
+
+	}
+}
